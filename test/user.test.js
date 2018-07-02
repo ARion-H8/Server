f5a46dd88d71133f29aae9d0c2ca858eb0f2e4bf
Faip Mardoni <faipmardoni@gmail.com>
2018-07-02 22:47:28 +0700

    test until add product

--- test/user.test.js
+++ test/user.test.js
@@ -3,13 +3,14 @@ const chaiHttp = require('chai-http');
 const expect   = chai.expect;
 const app      = require('../app');
 chai.use(chaiHttp);
+const userModel = require('../models/user.model')
 
-const email = 'hacktiv8@gmail.com';
+const email = 'test@mail.com';
 const password = 'hacktiv8';
-var token = '';
+var token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1YjNhMzczZGRhNDFlMzJiYTExMjY4NjYiLCJlbWFpbCI6InRlc3RAbWFpbC5jb20iLCJpYXQiOjE1MzA1NDE5NTl9.uwWehqRa9OKFxekqzZx1aQKgaWr0A5LSMYajV9ERayw';
 const newUser= {
-  username: "rey",
-  email: "rey@gmail.com",
+  username: "test",
+  email: "test@mail.com",
   password: "hacktiv8"
 }
 
@@ -22,6 +23,7 @@ describe('user resolvers', () => {
           mutation {
             signIn(email: "${email}", password: "${password}"){
               _id
+              email
               password
               email
               token
@@ -35,8 +37,32 @@ describe('user resolvers', () => {
         };
         expect(res.status).to.be.equal(200);
         expect(res.body.data).to.be.an('object');
-        expect(res.body.data.signIn.token).to.be.a('string');
-        token = res.body.data.signIn.token
+        done();
+      });
+  });
+
+  it('should failed to login', (done) => {
+    chai.request(app)
+      .post('/graphql')
+      .send({
+        query: `
+          mutation {
+            signIn(email: "${email}", password: "${password}1"){
+              _id
+              email
+              password
+              email
+              token
+            }
+          }
+        `
+      })
+      .end((err, res) => {
+        if(err){
+          throw err;
+        };
+        expect(res.status).to.be.equal(200);
+        // expect(res.body.data).to.be.an('undefined');
         done();
       });
   });
@@ -60,8 +86,59 @@ describe('user resolvers', () => {
         `
       })
       .end((err, res) => {
+        expect(res.status).to.be.equal(200);
+        expect(res.body.data).to.be.an('object');
+        done();
+      });
+  });
+
+  it('should failed register user ', (done) => {
+    chai.request(app)
+      .post('/graphql')
+      .send({
+        query:`
+        mutation {
+          signUp(newUser: {
+            username: "${newUser.username}",
+            email: "${newUser.email}",
+            password: "${newUser.password}"
+          }){
+            username
+            password
+            email
+          }
+        }
+        `
+      })
+      .end((err, res) => {
         if(err) {
           throw err.message;
+        };
+        expect(res.status).to.be.equal(200);
+        expect(res.body.data.signUp).to.be.equal(null);
+        done();
+      });
+  });
+
+  it('show user', (done) => {
+    chai.request(app)
+      .post('/graphql')
+      .set('Authorization', `Bearer ${ token }` )
+      .send({
+        query:`
+        query {
+          user {
+            _id
+            username
+            email
+            password
+          }
+        }
+        `
+      })
+      .end((err, res) => {
+        if(err) {
+          throw err;
         };
         expect(res.status).to.be.equal(200);
         expect(res.body.data).to.be.an('object');
@@ -69,31 +146,29 @@ describe('user resolvers', () => {
       });
   });
 
-  // it('register user', (done) => {
-  //   chai.request(app)
-  //     .post('/graphql')
-  //     .set('Authorization', `Bearer ${ token }` )
-  //     .send({
-  //       query:`
-  //       query {
-  //         products{
-  //           _id
-  //           name
-  //           price
-  //           image
-  //         }
-  //       }
-  //       `
-  //     })
-  //     .end((err, res) => {
-  //       if(err) {
-  //         throw err;
-  //       };
-  //       expect(res.status).to.be.equal(200);
-  //       expect(res.body.data).to.be.an('object');
-  //       done();
-  //     });
-  // });
+  it('failed get user', (done) => {
+    chai.request(app)
+      .post('/graphql')
+      .send({
+        query:`
+        query {
+          user {
+            _id
+            username
+            email
+            password
+          }
+        }
+        `
+      })
+      .end(async (err, res) => {
+        expect(res.body.data.user).to.be.equal(null);
+        // await userModel.findOneAndRemove({
+        //   email:email
+        // })
+        done();
+      });
+  });
 
   
 
